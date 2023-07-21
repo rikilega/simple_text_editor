622502550ea24230d5c68c5095b7e5d56b6a1cbb
rikilega <rosedoland33@yahoo.com>
2023-07-20 19:15:31 -0700

    modified index.js and webpack.config.js file

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,14 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js');
+        navigator.serviceWorker.register('/sw.js')
+            .then((registration) => {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, (err) => {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
     });
 }
 
@@ -39,3 +46,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
