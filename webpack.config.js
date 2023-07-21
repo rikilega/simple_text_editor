9025be6be7038b81162b8ffd60f02dd3782c4dcf
rikilega <rosedoland33@yahoo.com>
2023-07-20 20:36:24 -0700

    fixed webpack.config.js file so that filepaths are correct on the output

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,55 @@
+// const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+// module.exports = {
+//   entry: './client/src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js',
+//     chunkFormat: 'array-push'
+//   },
+//   target: 'web', // Updated target
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin(),
+//     new HtmlWebPackPlugin({
+//       template: path.resolve(__dirname, './client/public/index.html'),
+//       filename: "./index.html"
+//     }),
+//     new WorkboxPlugin.GenerateSW({
+//       clientsClaim: true,
+//       skipWaiting: true,
+//     }),
+//     new WebpackPwaManifest({
+//       fingerprints: false,
+//       inject: true,
+//       name: 'Text Editor',
+//       short_name: 'App',
+//       description: 'My awesome Progressive Web App!',
+//       background_color: '#ffffff',
+//       start_url: '/',
+//       icons: [{
+//         src: path.resolve('assets/icon.png'),
+//         sizes: [96, 128, 192, 256, 384, 512],
+//         destination: path.join('assets', 'icons')
+//       }]
+//     })
+//   ]
+// };
+
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
@@ -9,7 +61,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    chunkFormat: 'array-push'
+    chunkFormat: 'array-push',
+    publicPath: '/' // Add this line to set the publicPath for Webpack output
   },
   target: 'web', // Updated target
   module: {
@@ -27,7 +80,8 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: path.resolve(__dirname, './client/public/index.html'),
-      filename: "./index.html"
+      filename: "./index.html",
+      publicPath: '/' // Add this line to set the publicPath for HtmlWebpackPlugin
     }),
     new WorkboxPlugin.GenerateSW({
       clientsClaim: true,
@@ -49,4 +103,3 @@ module.exports = {
     })
   ]
 };
-
